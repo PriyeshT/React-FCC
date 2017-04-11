d6b753b1d1ee767e56573251354cbf1b351a12de
Priyesh Tungare <priyeshtungare@Priyeshs-MacBook-Pro.local>
2017-04-11 22:21:59 +0800

    tried to load Button module. Failed Attempt'

---
+++ lesson1/src/_modules/Button/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+class Button extends Component {
+	render() {
+		return {
+			<div className="button-group">
+			
+			</div>
+		};
+	}
+}
+
+export default Button;
